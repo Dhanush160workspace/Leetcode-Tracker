51f2c5e1b120c364049cdf01fc113fc37ec615d0
Dhanush160workspace <dhanushworkspace160@gmail.com>
2026-07-09 15:09:47 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 7/9/2026, 3:09:46 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
