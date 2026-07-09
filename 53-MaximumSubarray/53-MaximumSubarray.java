68dcd4f6b24d4b0f05d11d58657d8b0c9ec1d3cb
Dhanush160workspace <dhanushworkspace160@gmail.com>
2026-07-09 15:11:14 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 3:11:14 PM
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //Carens Algorithm
+        int ms = nums[0];
+        int cs = 0;
+
+        for (int i=0; i<nums.length; i++){
+            cs += nums[i];
+            if (cs > ms) ms = cs;
+            if (cs < 0) cs = 0;
+        }
+
+        return ms;
+    }
+}
