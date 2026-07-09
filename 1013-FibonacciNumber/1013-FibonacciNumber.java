b5c41d35b2f45aefeadf07b093cb17af26ef769f
Dhanush160workspace <dhanushworkspace160@gmail.com>
2026-07-09 15:08:48 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 3:08:48 PM
+class Solution {
+    public int fib(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
+    }
+    public int helper(int n, int dp[]){
+        if (n <=1){
+            dp[n] = n;
+            return n;
+        }
+
+        if (dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = helper(n-1, dp) + helper(n-2, dp);
+        return dp[n];
+    }
+}
