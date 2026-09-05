383701dfe3931922a84f6f8c82517e03c8bfef3e
Dhanush160workspace <dhanushworkspace160@gmail.com>
2026-09-05 19:43:50 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/version/java/202-HappyNumber_20260905_194350.java
@@ -0,0 +1,12 @@
+// Last updated: 9/5/2026, 7:43:50 PM
+1class Solution {
+2    public boolean isPowerOfTwo(int n){
+3        if (n<=0){
+4            return false;
+5        }
+6        while(n%2==0){
+7            n=n/2;
+8        }
+9        return n==1;
+10    }
+11}
