2d44b6c4624b554fb48f6ad7e871102506194013
Dhanush160workspace <dhanushworkspace160@gmail.com>
2026-09-05 21:12:55 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/version/java/202-HappyNumber_20260905_211255.java
@@ -0,0 +1,16 @@
+// Last updated: 9/5/2026, 9:12:55 PM
+1class Solution {
+2    public int numberOfSteps(int num) {
+3        int s = 0 ;
+4        while (num!=0){
+5            if (num%2 == 0){
+6                num = num/2;
+7                s++;
+8            } else {
+9                num--;
+10                s++;
+11            }
+12        }
+13        return s;
+14    }
+15}
